532e9527a6d4e0d40d6c33e1a1b21e097a303ce3
rathiakshay <akshay.rathi@intimetec.com>
2020-05-24 11:26:38 +0530

    assignment1V1

---
+++ conFusion/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  // template: `<h1>{{title}}</h1>`
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'conFusion';
+}
